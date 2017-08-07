c31b7d8d12777d646259463f01115dfac3dbc1b8
xuchao <85825770@qq.com>
2017-08-07 11:29:08 +0800

    短信API OK

--- src/Sms.php
+++ src/Sms.php
@@ -24,7 +24,7 @@ class Sms extends Rpc
     /**
      * @var string Api接口版本
      */
-    public $version = '1.0';
+    public $version = '2017-05-25';
 
     /**
      * @var string 短信签名
@@ -44,7 +44,8 @@ class Sms extends Rpc
     }
 
     /**
-     * @param string $phoneNumbers 短信接收号码。支持以逗号分隔的形式进行批量调用，批量上限为1000个手机号码,批量调用相对于单条调用及时性稍有延迟,验证码类型的短信推荐使用单条调用的方式
+     * 发送模板短信
+     * @param string $phoneNumbers 短信接收号码。支持以逗号分隔的形式进行批量调用，批量上限为1000个手机号码
      * @param string $signName 短信签名
      * @param string $templateCode 短信模板ID
      * @param array $templateParam 短信模板变量替换JSON串。
@@ -54,12 +55,33 @@ class Sms extends Rpc
     public function sendTemplateSms($phoneNumbers, $templateCode, array $templateParam = [], $signName = null, $outId = null)
     {
         return $this->get('', [
-            'Action' => 'ForbidLiveStream',
+            'Action' => 'SendSms',
             'PhoneNumbers' => $phoneNumbers,
-            'SignName' => $signName,
+            'SignName' => $signName ? $signName : $this->signName,
             'TemplateCode' => $templateCode,
             'TemplateParam' => Json::encode($templateParam),
             'OutId' => $outId
+        ]);
+    }
+
+    /**
+     * 短信查询API
+     * @param string $phoneNumber
+     * @param int $sendDate
+     * @param int $pageSize
+     * @param int $currentPage
+     * @param null $bizId
+     * @return array
+     */
+    public function querySendDetails($phoneNumber, $sendDate, $pageSize = 10, $currentPage = 1, $bizId = null)
+    {
+        return $this->get('', [
+            'Action' => 'QuerySendDetails',
+            'PhoneNumber' => $phoneNumber,
+            'BizId' => $bizId,
+            'SendDate' => $sendDate,
+            'PageSize' => $pageSize,
+            'CurrentPage' => $currentPage
         ]);
     }
 }
