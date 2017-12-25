bd3dbed253e79fd804a90b5482a28293706fddb1
Peter <xutongle@gmail.com>
2017-12-25 18:40:58 +0800

    API网关的外部访问接口

--- Api.php
+++ Api.php
@@ -7,6 +7,8 @@
 
 namespace xutl\aliyun;
 
+use yii\base\Exception;
+use yii\helpers\Url;
 use yii\httpclient\Client;
 use yii\httpclient\RequestEvent;
 use yii\base\InvalidConfigException;
@@ -50,9 +52,86 @@ class Api extends Client
      * 请求事件
      * @param RequestEvent $event
      * @return void
+     * @throws Exception
      */
     public function RequestEvent(RequestEvent $event)
     {
-        $params = $event->request->getData();
+        $headers = $event->request->getHeaders();
+        $headers->add('X-Ca-Key', $this->appKey);
+        $headers->add('X-Ca-Timestamp', strval(time() * 1000));
+        $headers->add('X-Ca-Nonce', uniqid());
+        $headers->add('X-Ca-Version', 1);
+        $headers->add('X-Ca-Stage','RELEASE');
+        $headers->add('Accept', 'application/json');
+
+        $signString = strtoupper($event->request->getMethod()) . "\n";
+        if ($headers->has('Accept')) {
+            $signString = $signString . $headers->get('Accept');
+        }
+        $signString = $signString . "\n";
+        if ($headers->has('Content-MD5')) {
+            $signString = $signString . $headers->get('Content-MD5');
+        }
+        $signString = $signString . "\n";
+        if ($headers->has('Content-Type')) {
+            $signString = $signString . $headers->get('Content-Type');
+        }
+        $signString = $signString . "\n";
+        if ($headers->has('Date')) {
+            $signString = $signString . $headers->get('Date');
+        }
+        $signString = $signString . "\n" . $this->buildCanonicalHeaders($headers->toArray());
+        $signString .= $this->composeUrl($event->request->getUrl(), $event->request->getData());
+
+        if ($this->signatureMethod == self::SIGNATURE_METHOD_HMACSHA256) {
+            $sign = base64_encode(hash_hmac('sha256', $signString, $this->appSecret, true));
+        } elseif ($this->signatureMethod == self::SIGNATURE_METHOD_HMACSHA1) {
+            $sign = base64_encode(hash_hmac('sha1', $signString, $this->appSecret, true));
+        } else {
+            throw new Exception('Unsupported signature method.');
+        }
+        $headers->add('X-Ca-Signature', $sign);
+        $event->request->setHeaders($headers);
+    }
+
+    /**
+     * Composes URL from base URL and GET params.
+     * @param string $url base URL.
+     * @param array $params GET params.
+     * @return string composed URL.
+     */
+    protected function composeUrl($url, $params = [])
+    {
+        if (!empty($params)) {
+            if (strpos($url, '?') === false) {
+                $url .= '?';
+            } else {
+                $url .= '&';
+            }
+            $url .= http_build_query($params, '', '&', PHP_QUERY_RFC3986);
+        }
+        return $url;
+    }
+
+    /**
+     * 构建规范 Headers
+     * @param array $headers
+     * @return string
+     */
+    private function buildCanonicalHeaders(array $headers)
+    {
+        $sortMap = [];
+        foreach ($headers as $headerKey => $headerValue) {
+            $key = strtolower($headerKey);
+            if (strpos($key, 'X-Ca-') === 0) {
+                $sortMap[$key] = $headerValue;
+            }
+        }
+        ksort($sortMap);
+        $headerString = '';
+        foreach ($sortMap as $sortMapKey => $sortMapValue) {
+            $headerString = $headerString . $sortMapKey . ':' . $sortMapValue . "\n";
+        }
+        return $headerString;
     }
 }
