b124ea6a10ef66cc82ed443115dcba7371446e95
Peter <xutongle@gmail.com>
2018-01-11 18:48:04 +0800

    update

---
+++ actions/ContainerAction.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+
+namespace xutl\aliyun\actions;
+
+use Yii;
+use yii\base\Action;
+use yii\helpers\Json;
+use yii\base\InvalidConfigException;
+
+/**
+ * 阿里云容器镜像webhook回调
+ * 需要Request 组件支持解析POST JSON
+ * @package xutl\aliyun\actions
+ */
+class ContainerAction extends Action
+{
+    /**
+     * @var callable success callback with signature: `function($params)`
+     */
+    public $callback;
+
+    /**
+     * 初始化
+     * @throws InvalidConfigException
+     */
+    public function init()
+    {
+        parent::init();
+        if (empty ($this->callback)) {
+            throw new InvalidConfigException ('The "callback" property must be set.');
+        }
+    }
+
+    /**
+     * 处理直播通知回调
+     */
+    public function run()
+    {
+        $params = Yii::$app->request->post();
+        Yii::info(Json::encode($params), __METHOD__);
+        return call_user_func($this->callback, $params);
+    }
+}
