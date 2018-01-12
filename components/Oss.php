20e2ec7b1807e57e6f001961c27a6b14fb46f782
Peter <xutongle@gmail.com>
2018-01-12 13:57:04 +0800

    加载AliOss

---
+++ components/Oss.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+
+namespace xutl\aliyun\components;
+
+/**
+ * Class Oss
+ * @package xutl\aliyun\components
+ */
+class Oss extends OssClient
+{
+
+}
